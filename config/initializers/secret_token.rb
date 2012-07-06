be91451f0078bc608d3c12a7a21808244d52a598
shu0115 <raisondetre0115@gmail.com>
2012-07-06 17:50:00 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roomel::Application.config.secret_token = 'f22c391da136d3b9dd674f8bb0d8092d095256c3c4106768a447874e93c72be5c55b6721b5a7dd4cc7cbab4db7907608d0f02f032ac97e404a176f7bd823be92'
